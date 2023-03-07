b9536cd76cad48b87fd3ebdf402024c738115dfd
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-03-07 09:24:37 -0600

    operaciones aritmeticas

--- Functions.java
+++ Functions.java
@@ -1,2 +1,58 @@
+import java.util.Stack;
+
 public class Functions {
+    Addition add= new Addition();
+    Division div= new Division();
+    Multiplication mult = new Multiplication();
+    Substraction sub= new Substraction();
+    
+    public String aritmetricas(String exp){
+        Stack<Integer> stack = new Stack<Integer>();
+
+        String[] tokens = exp.split("[() ]+");
+
+        for (int i = tokens.length - 1; i >= 0; i--) {
+            String token = tokens[i];
+
+            if (isNumeric(token)) {
+                stack.push(Integer.parseInt(token));
+            } else if(token.equals("+")||token.equals("-")||token.equals("*")||token.equals("/")||token.equals("mod")||token.equals("rem")){
+                int operand1 = stack.pop();
+                int operand2 = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(add.add(operand1, operand2));
+                        break;
+                    case "-":
+                        stack.push(sub.substract(operand1, operand2));
+                        break;
+                    case "*":
+                        stack.push(mult.multiply(operand1, operand2));
+                        break;
+                    case "/":
+                        stack.push(div.divide(operand1, operand2));
+                        break;
+                    case "mod":
+                        stack.push(operand1 % operand2);
+                        break;
+                    case "rem":
+                        stack.push(operand1 - (operand2 * (operand1 / operand2)));
+                        break;
+                }
+            }
+        }
+
+        return stack.pop().toString();
+    }
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
 }
--- Interpreter.java
+++ Interpreter.java
@@ -1,6 +1,29 @@
-
+import java.util.Scanner;
 
 public class Interpreter {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Boolean continuar= true;
+        Functions f= new Functions();
+        int op=0;
+        String exp= "";
+        System.out.println("Bienvenido al intérpreter de Lisp");
+        while(continuar){
+            System.out.println("¿Qué desea Hacer?");
+            System.out.println("1. Operaciones aritmétricas");
+            op= teclado.nextInt();
+            teclado.nextLine();
+            if(op==1){
+                System.out.println("Ingrese la operación");
+                exp= teclado.nextLine();
+                System.out.println(f.aritmetricas(exp));
+            }
+            else{
+                continuar=false;
+            }
+        }
+        
+    }
     
 }
 
