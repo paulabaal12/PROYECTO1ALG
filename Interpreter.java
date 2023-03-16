f476c670ab6c7028bf6691ab9a6408c433c6d833
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-03-16 10:41:30 -0600

    Cambios al main

--- Interpreter.java
+++ Interpreter.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Interpreter {
@@ -5,29 +6,44 @@ public class Interpreter {
         Scanner teclado = new Scanner(System.in);
         Boolean continuar= true;
         Functions f= new Functions();
+        ArrayList<Object> lista= new ArrayList<>();
         int op=0;
         String exp= "";
         System.out.println("Bienvenido al intérpreter de Lisp");
-        while(continuar){
-            System.out.println("¿Qué desea Hacer?");
-            System.out.println("1. Operaciones aritmétricas \n2. Predicados");
-            op= teclado.nextInt();
-            teclado.nextLine();
-            if(op==1){
-                System.out.println("Ingrese la operación");
-                exp= teclado.nextLine();
-                System.out.println(f.aritmetricas(exp));
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        while (count >= 0) {
+            String line = scanner.nextLine();
+            sb.append(line);
+            for (char c : line.toCharArray()) {
+                if (c == '(') {
+                    count++;
+                } else if (c == ')') {
+                    count--;
+                }
             }
-            else if( op==2){
-                System.out.println("Ingrese el predicado");
-                exp= teclado.nextLine();
-                System.out.println(f.predicados(exp));
-            }
-            else{
-                continuar=false;
+            if (count == 0) {
+                break;
+            } else {
+                sb.append("\n");
             }
         }
+        exp = sb.toString();
         
+        String exp2;
+        exp2= exp.replaceAll("[()]", "");
+        String[] tokens = exp2.split("[\\s']+");
+        String pred = tokens[0];
+        if(pred.equals("atom")||pred.equals("list")||pred.equals("equal")||pred.equals("<")||pred.equals(">")){
+            System.out.println(f.predicados(exp));
+        }
+        else if(pred.equals("setq")){
+            f.setq(exp);
+        }
+        else if(pred.equals("+")||pred.equals("-")||pred.equals("/")||pred.equals("*")){
+            System.out.println(f.aritmetricas(exp));
+        }
     }
     
 }
