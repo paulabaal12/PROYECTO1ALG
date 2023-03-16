7e91ef68f49bcf42fafb671f9d56055029d02bfd
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-03-16 10:46:43 -0600

    Cambios al main update

--- Interpreter.java
+++ Interpreter.java
@@ -9,41 +9,46 @@ public class Interpreter {
         ArrayList<Object> lista= new ArrayList<>();
         int op=0;
         String exp= "";
-        System.out.println("Bienvenido al intérpreter de Lisp");
+        System.out.println("Bienvenido al intérpreter de Lisp, escriba 'fin' para terminar de usar el programa");
         Scanner scanner = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
         int count = 0;
-        while (count >= 0) {
-            String line = scanner.nextLine();
-            sb.append(line);
-            for (char c : line.toCharArray()) {
-                if (c == '(') {
-                    count++;
-                } else if (c == ')') {
-                    count--;
+        while(!exp.equals("fin")){
+            StringBuilder sb = new StringBuilder();
+            while (count >= 0) {
+                
+                String line = scanner.nextLine();
+                sb.append(line);
+                for (char c : line.toCharArray()) {
+                    if (c == '(') {
+                        count++;
+                    } else if (c == ')') {
+                        count--;
+                    }
+                }
+                if (count == 0) {
+                    break;
+                } else {
+                    sb.append("\n");
                 }
             }
-            if (count == 0) {
-                break;
-            } else {
-                sb.append("\n");
+            exp = sb.toString();
+            
+            String exp2;
+            exp2= exp.replaceAll("[()]", "");
+            String[] tokens = exp2.split("[\\s']+");
+            String pred = tokens[0];
+            if(pred.equals("atom")||pred.equals("list")||pred.equals("equal")||pred.equals("<")||pred.equals(">")){
+                System.out.println(f.predicados(exp));
             }
+            else if(pred.equals("setq")){
+                f.setq(exp);
+            }
+            else if(pred.equals("+")||pred.equals("-")||pred.equals("/")||pred.equals("*")){
+                System.out.println(f.aritmetricas(exp));
+            }
+
         }
-        exp = sb.toString();
         
-        String exp2;
-        exp2= exp.replaceAll("[()]", "");
-        String[] tokens = exp2.split("[\\s']+");
-        String pred = tokens[0];
-        if(pred.equals("atom")||pred.equals("list")||pred.equals("equal")||pred.equals("<")||pred.equals(">")){
-            System.out.println(f.predicados(exp));
-        }
-        else if(pred.equals("setq")){
-            f.setq(exp);
-        }
-        else if(pred.equals("+")||pred.equals("-")||pred.equals("/")||pred.equals("*")){
-            System.out.println(f.aritmetricas(exp));
-        }
     }
     
 }
