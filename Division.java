40bb15ce8d2450d08476036e23e8e081cb547569
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-03-18 12:12:57 -0500

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,5 +1,11 @@
 //Paula Barillas, Ana Paula Navas, Nicolle Gordillo
 public class Division {
+    
+    /** 
+     * @param a
+     * @param b
+     * @return int
+     */
     public int divide(int a, int b){
 
         return a/b;
