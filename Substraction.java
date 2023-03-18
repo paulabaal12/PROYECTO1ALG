47e2c444659b3f76c305b69b26a6a9dba3138949
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-03-18 12:14:43 -0500

    Update Substraction.java

--- Substraction.java
+++ Substraction.java
@@ -1,5 +1,11 @@
 //Paula Barillas, Ana Paula Navas, Nicolle Gordillo
 public class Substraction {
+    
+    /** 
+     * @param a
+     * @param b
+     * @return int
+     */
     public int substract(int a, int b){
         return a-b;
     }
