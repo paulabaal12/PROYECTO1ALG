001d63f8b1e27d3eb4184e03e15f89994eff4115
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-03-18 12:13:45 -0500

    javadoc

--- Addition.java
+++ Addition.java
@@ -1,6 +1,12 @@
 //Paula Barillas, Ana Paula Navas, Nicolle Gordillo
 
 public class Addition {
+    
+    /** 
+     * @param a
+     * @param b
+     * @return int
+     */
     public int add(int a, int b){
         return a+b;
     }
--- Multiplication.java
+++ Multiplication.java
@@ -1,5 +1,11 @@
 //Paula Barillas, Ana Paula Navas, Nicolle Gordillo
 public class Multiplication {
+    
+    /** 
+     * @param a
+     * @param b
+     * @return int
+     */
     public int multiply(int a, int b){
         return a*b;
     }
